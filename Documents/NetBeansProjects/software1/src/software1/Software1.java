aaef0fbf40fc59a120aba151452a5bf58432790a
rgtoa <>
2023-01-30 16:54:15 +0800

    QNB Project

---
+++ Documents/NetBeansProjects/software1/src/software1/Software1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package software1;
+
+/**
+ *
+ * @author 97433
+ */
+public class Software1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
