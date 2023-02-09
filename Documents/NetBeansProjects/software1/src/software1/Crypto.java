762b6d9b5720687bdb06db092b49d476e136ec43
rlribaya <79568279+rlribaya@users.noreply.github.com>
2023-02-09 13:47:49 +0800

    Create Crypto.java

---
+++ Documents/NetBeansProjects/software1/src/software1/Crypto.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package software1;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import org.apache.commons.codec.binary.Base64;
+
+/**
+ *
+ * @author POLO
+ */
+public class Crypto {
+    static byte[] key = { 
+        'q','n','b','w','a','t','e','r',
+        's','o','f','t','e','n','g','2'
+    };
+    public static String encrypt(String strToEncrypt) {
+        String encryptedString = null;
+        try {
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            final SecretKeySpec secretKey = new SecretKeySpec(key, "AES");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            encryptedString = Base64.encodeBase64String(cipher.doFinal(strToEncrypt.getBytes()));
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+        return encryptedString;
+    }
+
+    public static String decrypt(String codeDecrypt) {
+        String decryptedString = null;
+        try {
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+            final SecretKeySpec secretKey = new SecretKeySpec(key, "AES");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            decryptedString = new String(cipher.doFinal(Base64.decodeBase64(codeDecrypt)));
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+        return decryptedString;
+    }
+}
