dd477837da5f47be90d7c06e61ea513a8ba360a2
rlribaya <79568279+rlribaya@users.noreply.github.com>
2023-02-28 19:00:51 +0800

    Update Crypto.java

--- Documents/NetBeansProjects/software1/src/software1/Crypto.java
+++ Documents/NetBeansProjects/software1/src/software1/Crypto.java
@@ -3,6 +3,7 @@ package software1;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
+import org.apache.commons.codec.binary.Base64;
 //import org.apache.commons.codec.binary.Base64;
 
 public class Crypto {
@@ -16,7 +17,7 @@ public class Crypto {
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
             final SecretKeySpec secretKey = new SecretKeySpec(key, "AES");
             cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-            //encryptedString = Base64.encodeBase64String(cipher.doFinal(strToEncrypt.getBytes()));
+            encryptedString = Base64.encodeBase64String(cipher.doFinal(strToEncrypt.getBytes()));
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
@@ -29,10 +30,26 @@ public class Crypto {
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
             final SecretKeySpec secretKey = new SecretKeySpec(key, "AES");
             cipher.init(Cipher.DECRYPT_MODE, secretKey);
-            //decryptedString = new String(cipher.doFinal(Base64.decodeBase64(codeDecrypt)));
+            decryptedString = new String(cipher.doFinal(Base64.decodeBase64(codeDecrypt)));
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
         return decryptedString;
+    }
+    
+    public static void main(String[] args) {
+        System.out.print("Choose Method:\n"
+                + "[1] Encrypt\n"
+                + "[2] Decrypt\n> ");
+        java.util.Scanner s = new java.util.Scanner(System.in);
+        if(s.nextInt() == 1) {
+            System.out.print("Enter text to encrypt:\n> ");
+            String text = s.next();
+            System.out.println("Given Text: " + text);
+            System.out.println("Encrypted Text: " + encrypt(text));
+        } else {
+            System.out.print("Enter text to decrypt:\n> ");
+            System.out.println("Encrypted Text: " + decrypt(s.next()));
+        }
     }
 }
